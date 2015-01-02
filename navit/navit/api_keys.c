85c5854e6241cf5cde73f3724eb78eab96dcb710
Pierre GRANDIN <grandinp@altern.org>
2015-01-02 14:25:00 -0800

    Added echonest api keys stubs

--- navit/navit/api_keys.c
+++ navit/navit/api_keys.c
@@ -8,6 +8,10 @@ char *foursquare_client_id = "";
 char *foursquare_client_secret = "";
 char *foursquare_oauth_token = "";
 
+char *echonest_api_key= "";
+char *echonest_consumer_key= "";
+char *echonest_shared_secret= "";
+
 const uint8_t spotify_apikey[] = {
 };
 const size_t spotify_apikey_size = sizeof(spotify_apikey);
