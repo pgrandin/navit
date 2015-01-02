8d534f317524f4336242e573c6c86f2a681321cf
Pierre GRANDIN <grandinp@altern.org>
2015-01-02 12:48:41 -0800

    Moving spotify code to the main folder, including the media stub functions

--- navit/navit/navit.c
+++ navit/navit/navit.c
@@ -75,6 +75,9 @@
 #ifdef HAVE_API_WIN32_CE
 #include "libc.h"
 #endif
+#ifdef USE_SPOTIFY
+#include "spotify.h"
+#endif
 
 /* define string for bookmark handling */
 #define TEXTFILE_COMMENT_NAVI_STOPPED "# navigation stopped\n"
@@ -2150,6 +2153,8 @@ navit_init(struct navit *this_)
 #if 0
 	routech_test(this_);
 #endif
+	spotify_set_attr(this_->gui);
+	spotify_navit_init (this_);
 }
 
 void
