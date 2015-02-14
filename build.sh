eb1618a8c7bfa1405b437aecfc3f21919ac1d50a
Pierre GRANDIN <grandinp@altern.org>
2015-02-14 15:52:48 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export PATH=~/navit/navit/cov-analysis-linux64-7.6.0/bin:$PATH
 
 mkdir bin && cd bin
 cov-build --dir cov-int cmake ../
-cov-build --dir cov-int make -j32
+cov-build --dir cov-int make -j32 || exit -1
 tar czvf navit.tgz cov-int
 
 curl --form token=$COVERITY_TOKEN \
