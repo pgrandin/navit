8d43f95927298ef282ef5c01212815c8feaf7b43
Pierre GRANDIN <grandinp@altern.org>
2015-02-19 03:14:13 -0800

    Update test_route.sh

--- test_route.sh
+++ test_route.sh
@@ -3,7 +3,9 @@ lng="-122.399168"
 dlat="37.839374"
 dlng="-122.289420"
 
+echo '<map type="binfile" data="~/assets/california-latest.bin" />' > ~/navit/navit/navit/bin/navit/maps/planet.xml
 
+dbus-send  --print-reply --session --dest=org.navit_project.navit /org/navit_project/navit/default_navit org.navit_project.navit.navit.set_center_by_string string:"geo: $lng $lat"
 dbus-send  --print-reply --session --dest=org.navit_project.navit /org/navit_project/navit/default_navit org.navit_project.navit.navit.set_position string:"geo: $lng $lat"
 dbus-send  --print-reply --session --dest=org.navit_project.navit /org/navit_project/navit/default_navit org.navit_project.navit.navit.set_destination string:"geo: $dlng $dlat" string:"dbus"
 sleep 1
