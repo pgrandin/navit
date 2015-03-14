26076dcbfd58f0a864c646f389a5ff1b83b212dd
Pierre GRANDIN <grandinp@altern.org>
2015-03-14 14:30:06 -0700

    Adding a new test case

--- test_route.sh
+++ test_route.sh
@@ -10,4 +10,15 @@ dbus-send  --print-reply --session --dest=org.navit_project.navit /org/navit_pro
 dbus-send  --print-reply --session --dest=org.navit_project.navit /org/navit_project/navit/default_navit org.navit_project.navit.navit.set_destination string:"geo: $dlng $dlat" string:"dbus"
 sleep 1
 import -window root $1/route.png
-dbus-send --session --type=method_call --print-reply --dest=org.navit_project.navit /org/navit_project/navit/default_navit org.navit_project.navit.navit.export_as_geojson string:$1/highfive_test.geojson
+dbus-send --session --type=method_call --print-reply --dest=org.navit_project.navit /org/navit_project/navit/default_navit org.navit_project.navit.navit.export_as_geojson string:$1/h5_001.geojson
+
+lat="37.839753"
+lng="-122.297752"
+dlat="37.829220"
+dlng="-122.294190"
+dbus-send  --print-reply --session --dest=org.navit_project.navit /org/navit_project/navit/default_navit org.navit_project.navit.navit.set_center_by_string string:"geo: $lng $lat"
+dbus-send  --print-reply --session --dest=org.navit_project.navit /org/navit_project/navit/default_navit org.navit_project.navit.navit.set_position string:"geo: $lng $lat"
+dbus-send  --print-reply --session --dest=org.navit_project.navit /org/navit_project/navit/default_navit org.navit_project.navit.navit.set_destination string:"geo: $dlng $dlat" string:"dbus"
+sleep 1
+import -window root $1/h5.png
+dbus-send --session --type=method_call --print-reply --dest=org.navit_project.navit /org/navit_project/navit/default_navit org.navit_project.navit.navit.export_as_geojson string:$1/h5_002.geojson
